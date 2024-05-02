5d1e2dbff8d2fc0df1d85a5aa05648c95ffe773f
denisfl1 <denisfl1@hotmail.com>
2024-05-02 13:09:38 -0300

    update

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -9,17 +9,32 @@ import Header from "@/components/header";
 export default function studentsList(){
 
     const [students,setStudents] = useState<string[]>([])
+ 
+
+    const color=(student:any)=>{
+        let sum = 0
+
+        for(let i in student['notes']){
+
+        sum+=student['notes'][i] / 13
+
+        }
+       
+       
+    if(sum > 8 ) return "#32CD32" 
+    if(sum == 7 || 8 ) return "#4169E1"
+    if(sum <5 ) return "#FF0000"
+
+     
+    }
 
     useEffect(()=>{
 
         API.get('/getStudent').then(
-            res=>{
-                
-                setStudents(res.data)
-                console.log(res.data)
+            res=>{         
+                setStudents(res.data)  
             }
         )
-
 
     },[])
 
@@ -31,19 +46,51 @@ export default function studentsList(){
             <Header></Header>
 
             <div>
-        {students.map((student: any) => (
+            <table className="studentsTable">
+            <thead>
+
+                <tr>
+                    <th>RA</th>
+                    <th>Nome</th>
+                    <th>Sobrenome</th>            
+                    <th>Nota Final</th>
+                    <th>Ver todas</th>
+                 
+
+
+                </tr>
+
+                    
+            </thead>
+
+            <tbody>
+
+            {students.map((student: any) => {
+            const notes = (()=>{
+                let sum = 0
+                for(let i in student.notes){
+                    sum+= student.notes[i] / 13
+                }
+            return Math.round(sum)
+            })
+        
+            return (
+                
+            <tr key={student.id}>
             
+            <td>{student.ra}</td>    
+            <td>{student.name}</td>
+            <td>{student.surname}</td>
+            <td className="text-center" style={{backgroundColor:color(student)}}>{notes()}</td>
+            <td><button>Ver</button></td>
+         
+        </tr>
+      )})}
 
-            <ul>
-                <li>
-                    {student.name} {student.surname}
-                </li>
-            
+            </tbody>
 
-            </ul>
-
-      ))}
-              
+ 
+            </table>
             </div>
 
         </div>
