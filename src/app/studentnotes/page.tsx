c3a41098a1d50ee8e0857958715dae2c64785610
denisfl1 <denisfl1@hotmail.com>
2024-05-03 23:59:53 -0300

    configs

---
+++ src/app/studentnotes/page.tsx
@@ -0,0 +1,57 @@
+
+
+
+
+export default function StudentNotes (){
+
+    <div>
+
+<table className=" w-full">
+            <thead>
+
+                <tr>
+                    <th>RA</th>
+                    <th>Nome</th>
+                    <th>Sobrenome</th>            
+                    <th className="text-center">Média</th>
+                    <th className="text-center">Todas</th>
+                 
+                </tr>
+
+                    
+            </thead>
+
+            <tbody>
+        
+                <tr>
+
+                        <td>1</td>
+
+                        <td>2</td>
+
+
+                        <td>3</td>
+
+                        <td>4</td>
+
+                        <td>5</td>
+
+
+                </tr>
+
+            </tbody>
+
+ 
+            </table>
+
+
+
+
+
+    </div>
+
+
+
+
+
+}
