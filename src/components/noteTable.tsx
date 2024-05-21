71589bc531ad510acd7e7141dbd8c6658ff37082
denisfl1 <denisfl1@hotmail.com>
2024-05-20 23:51:37 -0300

    updateTable

--- src/components/noteTable.tsx
+++ src/components/noteTable.tsx
@@ -6,20 +6,12 @@ export default function NoteTable(props:{data:any}){
 
     const [studentDATA,setStudentDATA] = useState<any>()
 
-    // const [verify,setVerify] = useState()
-    // const [disc,setDisc ]= useState()
-    // const [first,setFirst]= useState()
-    // const [second,setSecond]= useState()
-    // const [third,setThird] = useState()
-    // const [fourth,setFourth] = useState()
-    // const [total,setTotal] = useState()
-
-    let disc =  ' '
+    let disc:any =  ''
     let first:any =  ''
     let second:any =  ''
     let third:any =  ''
     let fourth:any = ''
-    let total:any = null
+    let total:any = ''
 
     const subjects = [
         'Primeiro',
@@ -31,14 +23,14 @@ export default function NoteTable(props:{data:any}){
     ]
 
 
-    useEffect(()=>{
-        
-  
-
-    },[props.data])
-
-
-
+    const verify = props.data
+    disc = verify ? props.data.discipline : ''
+    first = verify ? props.data.notes['1'][disc] : ''
+    second = verify ?props.data.notes['2'][disc] : ''
+    third = verify ? props.data.notes['3'][disc] : ''
+    fourth = verify ? props.data.notes['4'][disc] : ''
+    total = Math.round((first + second + third + fourth) / 4)
+    const all = [first,second,third,fourth,total]
 
 
 return(
@@ -62,22 +54,14 @@ return(
 
             <tbody>
                 {subjects.map((it:any,index:number)=>{
-                          const verify = props.data
-                           disc = verify ? props.data.discipline : ' '
-                           first = verify ? props.data.notes['1'][disc] : ''
-                           second = verify ?props.data.notes['2'][disc] : ''
-                           third = verify ? props.data.notes['3'][disc] : ''
-                           fourth = verify ? props.data.notes['4'][disc] : ''
-                           total = verify ? Math.round((first + second + third + fourth) / 4) : ''
-                           const all = [first,second,third,fourth,total]
-                
+                      
+                           
                     return(
 
                         <>
                         <tr>
                             <td>{it}</td>                       
-                            <td>{all[index]}</td>
-                          
+                            <td>{all[index]}</td>                      
                         </tr>
                         </>
                    
